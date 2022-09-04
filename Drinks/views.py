479baba4c8b047964bf354f52166035f13287e96
yuran <yuran415@gmail.com>
2022-09-04 21:16:00 +0200

    Added my project

---
+++ Drinks/views.py
@@ -0,0 +1,10 @@
+import http
+from django.http import JsonResponse
+from .models import Drink
+from .serializers import DrinkSerializer
+
+
+def drink_list(request):
+    #get all the drinks
+    #serialize them
+    #return Json
